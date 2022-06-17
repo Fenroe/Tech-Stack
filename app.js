69ace571cb71c40f7c3c98b1d366d304b9a772ca
Fenroe <scrambles9494@gmail.com>
2022-06-17 16:53:12 +0300

    configured mongoose and added .env variables

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
@@ -8,6 +10,20 @@ const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express()
+
+//Import the mongoose module
+const mongoose = require('mongoose')
+
+//Set up default mongoose connection
+const mongoDB = process.env.MONGODB_URI
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+
+//Get the default connection
+const db = mongoose.connection
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
